627c8a93cf6d9dad3f20db8ae7e4e5cd75224e32
otradovec <otradovec@github.com>
2022-10-25 15:30:52 +0200

    Added pip requirements

--- code/api.py
+++ code/api.py
@@ -27,11 +27,10 @@ def connect():
 
 
 if __name__ == '__main__':
-	header = connect()
-	url = f"https://demo.flowmon.com/rest/ads/events?count=1&orderBy=%5B%22time%20desc%22%5D&search=%7B%0A%20%20%22from%22%3A%20%222020-07-24%2011%3A00%22%2C%0A%20%20%22to%22%3A%20%222022-07-25%2011%3A00%22%0A%7D"
-        data = requests.get(url, headers=header, verify=False)
-
-        f = open(f"ismdata.json", "w")
-        f.write(data.text)
-        f.close()
+    header = connect()
+    url = f"https://demo.flowmon.com/rest/ads/events?count=1&orderBy=%5B%22time%20desc%22%5D&search=%7B%0A%20%20%22from%22%3A%20%222020-07-24%2011%3A00%22%2C%0A%20%20%22to%22%3A%20%222022-07-25%2011%3A00%22%0A%7D"
+    data = requests.get(url, headers=header, verify=False)
+    f = open(f"ismdata.json", "w")
+    f.write(data.text)
+    f.close()
 
